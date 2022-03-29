200b0f674ff1de4083f83caba3044734c995fbb3
Евгений Салахутдинов <83494253+EugeneSal@users.noreply.github.com>
2022-03-30 02:48:50 +0800

    альфа-версия 0.77 воскрешение и статистика

--- hackaton/game/forms.py
+++ hackaton/game/forms.py
@@ -6,19 +6,19 @@ from .models import *
 class PaladinForm(forms.ModelForm):
     class Meta:
         model = Paladin
-        fields = ('name', 'nick_name', )
+        fields = ('name', 'nick_name', 'image')
 
 
 class WarriorForm(forms.ModelForm):
     class Meta:
         model = Warrior
-        fields = ('name', 'nick_name', )
+        fields = ('name', 'nick_name', 'image')
 
 
 class DruidForm(forms.ModelForm):
     class Meta:
         model = Druid
-        fields = ('name', 'nick_name', )
+        fields = ('name', 'nick_name', 'image')
 
 
 class ThingsForm(forms.ModelForm):
